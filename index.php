bd38080883b83afe8847a6306fbf29028119e00b
djimairo <djimairo2007@hotmail.com>
2023-09-08 10:07:42 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$naam = "Djimairo";
+$Studie ="Associate Degree Software Development";
+$Woonplaats ="Lelystad";
+$email = "djimairopinda99@hotmail.com";
+$telefoonnummer = "0664544883"
+$rijbewijs = false;
+$hobbies =
+$geboortedatum =
+$nationaliteit =
+$talen =
